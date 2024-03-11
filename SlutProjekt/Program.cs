8d71cfefb72bf52c6db031989881897facc7b364
Phatchara Champathong <phatchara.champathong@elev.ga.ntig.se>
2024-03-11 09:29:32 +0100

    asdasd

--- SlutProjekt/Program.cs
+++ SlutProjekt/Program.cs
@@ -1,5 +1,53 @@
 ﻿using System.Runtime.CompilerServices;
 
 
-System.Console.WriteLine("Please let me know your name:");
+
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("_________________________________________________________\n");
+System.Console.WriteLine("Warning: Do not press any key while dialog is processing.");
+Console.WriteLine("_________________________________________________________");
+Console.ResetColor();
+
+
+
+
+
+System.Console.WriteLine("\nYou wake up, look at the unusual ceiling.");
+System.Console.WriteLine("You raise up your body.");
+
+System.Console.WriteLine("You feel uneasy feeling in your wrist");
+Thread.Sleep(1000);
+Thread.Sleep(100);
+
+Console.WriteLine("\n                    ....:--..:                    :..--.....                            ");
+Thread.Sleep(100);
+Console.WriteLine("                     : ..::... ::                  .: : :::.  :                           ");
+Thread.Sleep(100);
+Console.WriteLine("                    .:.    :  :  -                -  :  :   .--                           ");
+Thread.Sleep(100);
+Console.WriteLine("                    --   .:...... .              : ......:.   +                           ");
+Thread.Sleep(100);
+Console.WriteLine("                    :    ...::.  :.              :.  .:....    :                          ");
+Thread.Sleep(100);
+Console.WriteLine("                    :      ..:.. :               : ...- :      :                          ");
+Thread.Sleep(100);
+Console.WriteLine("                   :       .  ..-                 .:.. :      .                           ");
+Thread.Sleep(100);
+Console.WriteLine("                   :           ..                  :          :                           ");
+Thread.Sleep(100);
+Console.WriteLine("                 .......... ..:.                    .:.:.::...-.                          ");
+Thread.Sleep(100);
+Console.WriteLine("                -              ..                   ..:         .:                        ");
+Thread.Sleep(100);
+Console.WriteLine("                -               :                 . -             :                       ");
+Thread.Sleep(100);
+Console.WriteLine("               :                ::.             -+=+=.            .                       ");
+Thread.Sleep(100);
+Console.WriteLine("               :                ::=+.:.::..-:-:--=.=--             :                      ");
+Thread.Sleep(100);
+Console.WriteLine("              .                 :   .-:--....::: ::. :   .......    .                     ");
+Thread.Sleep(100);
+Console.WriteLine("              - .:        .... ..                    - :.        .- :                     ");
+
 Console.ReadLine();
