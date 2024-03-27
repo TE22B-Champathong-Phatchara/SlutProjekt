775f442272295fea18b2ef851b43ff7852f6f3af
Phatchara Champathong <phatchara.champathong@elev.ga.ntig.se>
2024-03-27 11:16:14 +0100

    qwetry

--- SlutProjekt/Fighter.cs
+++ SlutProjekt/Fighter.cs
@@ -1,21 +1,48 @@
+
 using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 
 class Char
 {
     public int HP;
+    public int BaseHP;
     public int ATK;
-    public string GetName;
 
     public int AGI;
+    public string GetName;
+    private int ChanceToMiss;
+    private int Chance;
 
-    public void IsDead()
+    public int SPD;
+
+    public bool IsDead()
     {
         if(HP <= 0)
         {
             System.Console.WriteLine($"{GetName} Is Dead!");
+            return true;
         }
+        return false;
+    }
+
+    public bool Missed()
+    {
+        
+        if(SPD > AGI)
+        {
+            Chance = SPD - AGI;
+            ChanceToMiss = Random.Shared.Next(0,Chance);
+            
+            if (ChanceToMiss > 5)
+            {
+                System.Console.WriteLine("Missed!");
+                return true;
+            }   
+            
+        }
+        return false;
     }
 
 
--- SlutProjekt/Program.cs
+++ SlutProjekt/Program.cs
@@ -1,7 +1,12 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Data.Common;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
+using System.Net;
 using System.Runtime.CompilerServices;
 using System.Threading.Channels;
 
+
+Char PlStat = new Char();
 string name;
 string yn;
 
@@ -13,7 +18,8 @@ bool mag = false;
 
 int Floor = 1;
 
-bool EnemyIsDead = false;
+bool GameOver = true;
+
 
 string Command;
 
@@ -26,182 +32,241 @@ Console.ResetColor();
 Thread.Sleep(1000);
 System.Console.WriteLine("Welcome to Rouge Path!");
 System.Console.WriteLine("The goal of this game is reach the 100th floor of the Hopeless Tower!!");
-System.Console.WriteLine("Let's give a name of hero who dares to beat the tower:\n");
-while(true)
+System.Console.WriteLine("Let's give a name for the hero who dares to beat the tower:\n");
+
+
+
+
+
+
+
+bool NameCon = false;
+while (true)
 {
-    name = Console.ReadLine();
-
-    System.Console.WriteLine($"\nIs your name '{name}'?\n");
-
-    yn = Console.ReadLine().ToLower();
-
-    if(yn == "y" || yn == "yes")
+    
+    while(!NameCon)
     {
-        System.Console.WriteLine($"\nAlright {name} choose your class!\n");
-        break;
-    }
-    else if (yn == "n" || yn == "no")
-    {
-        System.Console.WriteLine("\nLet's write it again.\n");
-        continue;
-    }
-    else
-    {
-        System.Console.WriteLine("\n Please answer the question.\n");
-        continue;
-    }
-
-}
-
-
-
-
-
-
-
-
-while(true)
-{
-    List<string> classes = new List<string>(["Adventurer" , "Mage" , "Archer"]);
-
-    if (undead == false){
-        classes.Add("LOCKED");
-    }
-    if (undead == true)
-    {
-        classes.Add("Undead");
-    }
-
-    foreach (var role in classes)
-    {
-        System.Console.WriteLine(role + "\n");
-    }
-
-
-
-    choose = Console.ReadLine().ToLower();
-
-    if(choose == "adventurer" || choose.Contains("ad"))
-    {
-        System.Console.WriteLine("\nThe Adventurer class specializes in physical melee attacks as its main source of damage. You can't use magic; instead, you can use items to deal magic damage.");
-        System.Console.WriteLine("As a starting bonus, you receive 2x small healing potions (each healing 20 HP).");
-        System.Console.WriteLine("Are you sure you want to start as the Adventurer?");
-
-        yn = Console.ReadLine().ToLower();
-
-        if (yn.Contains('y') || yn == "yes")
-        {
-            System.Console.WriteLine("Alright, let's start");
-            adv = true;
-            break;
-        }
-        else if (yn.Contains('n') || yn == "no")
-        {
-            System.Console.WriteLine("Alright, let's decide again.");
-            continue;
-        }
-    }
-
-    else if(choose == "mage" || choose.Contains("ma"))
-    {
-        System.Console.WriteLine("The Mage class specializes in magic attacks");
         
-        yn = Console.ReadLine().ToLower();
-
-        if (yn.Contains('y') || yn == "yes")
+        
+        name = Console.ReadLine();
+        PlStat.GetName = name;
+        while(true)
         {
-            System.Console.WriteLine("Alright, let's start");
-            mag = true;
+            System.Console.WriteLine($"\nIs your name '{name}'?\n");
+
+            yn = Console.ReadLine().ToLower();
+
+            if(yn == "y" || yn == "yes")
+            {
+                System.Console.WriteLine($"\nAlright {name} choose your class!\n");
+                NameCon = true;
+                break;
+            }
+            else if (yn == "n" || yn == "no")
+            {
+                System.Console.WriteLine("\nLet's write it again.\n");
+                break;
+            }
+            else
+            {
+                System.Console.WriteLine("\nPlease answer the question.\n");
+                continue;
+            }
+        }
+        if(NameCon)
+        {
             break;
         }
-        else if (yn.Contains('n') || yn == "no")
+    }
+
+    GameOver = false;
+
+    while(true)
+    {
+        List<string> classes = new List<string>(["Adventurer" , "Mage" , "Archer"]);
+
+        if (undead == false){
+            classes.Add("LOCKED");
+        }
+        if (undead == true)
         {
-            System.Console.WriteLine("Alright, let's decide again.");
+            classes.Add("Undead");
+        }
+
+        foreach (var role in classes)
+        {
+            System.Console.WriteLine(role + "\n");
+        }
+
+
+
+        choose = Console.ReadLine().ToLower();
+
+        if(choose == "adventurer" || choose.Contains("ad"))
+        {
+            System.Console.WriteLine("\nThe Adventurer class specializes in physical melee attacks as its main source of damage. You can't use magic; instead, you can use items to deal magic damage.");
+            System.Console.WriteLine("As a starting bonus, you receive 2x small healing potions (each healing 20 HP).");
+            System.Console.WriteLine("Are you sure you want to start as the Adventurer?");
+
+            yn = Console.ReadLine().ToLower();
+
+            if (yn.Contains('y') || yn == "yes")
+            {
+                System.Console.WriteLine("Alright, let's start");
+                adv = true;
+                break;
+            }
+            else if (yn.Contains('n') || yn == "no")
+            {
+                System.Console.WriteLine("Alright, let's decide again.");
+                continue;
+            }
+        }
+
+        else if(choose == "mage" || choose.Contains("ma"))
+        {
+            System.Console.WriteLine("The Mage class specializes in magic attacks");
+            
+            yn = Console.ReadLine().ToLower();
+
+            if (yn.Contains('y') || yn == "yes")
+            {
+                System.Console.WriteLine("Alright, let's start");
+                mag = true;
+                break;
+            }
+            else if (yn.Contains('n') || yn == "no")
+            {
+                System.Console.WriteLine("Alright, let's decide again.");
+                continue;
+            }
+        }
+
+        else if(choose == "name")
+        {
+            System.Console.WriteLine("\nDo you wish to change your name?\n");
+            
+            yn = Console.ReadLine().ToLower();
+
+            if (yn.Contains('y') || yn == "yes")
+            {
+                NameCon = false;
+                break;
+            }
+            else if (yn.Contains('n') || yn == "no")
+            {
+                continue;
+            }
+
+
+        }
+        else{
+            System.Console.WriteLine($"\nThe class you provide is unavailable. If you wish to change your name type 'name'.\n");
             continue;
         }
+
     }
 
-    else{
-        System.Console.WriteLine("Ivalid input.");
+    
+
+    Char Enemy = new Char();
+
+    if(GameOver)
+    {
         continue;
     }
-
-}
-
-Char PlStat = new Char();
-
-Char Enemy = new Char();
-
-
-if (adv == true)
-{
     
-
-
-    PlStat.GetName = name;
-    PlStat.AGI = 10;
-    PlStat.HP = 100;
-    
-
-    while(Floor != 100)
+    if (adv == true)
     {
-        if (Floor == 1)
+
+        PlStat.SPD = 10;
+        PlStat.BaseHP = 100;
+        PlStat.HP = 100;
+        PlStat.AGI = 20;
+        PlStat.SPD = 10;
+
+        while (Floor != 100 && GameOver == false)
         {
-            Enemy.GetName = "The Rat";
-            Enemy.HP = 20;
+
+            bool EnemyIsDead = false;
+            bool HeroIsdead;
+            Enemy.HP = 20 + (Floor - 1) * 10;
+            Enemy.BaseHP = 20 + (Floor - 1) * 10;
+            Enemy.SPD = 12;
             Enemy.AGI = 12;
+            Enemy.GetName = "The Rat";
+            
+            
+            
+            System.Console.WriteLine($"Floor: {Floor}.\n");
+            System.Console.WriteLine($"You've faced {Enemy.GetName}\n");
+            System.Console.WriteLine($"{Enemy.GetName}'s HP : [{Enemy.HP}/{Enemy.BaseHP}]");
 
-            System.Console.WriteLine($"You've faced {Enemy.GetName}");
-
-            while(EnemyIsDead == false)
+            while (true)
             {
-                PlStat.ATK = Random.Shared.Next(10,15);
-                PlStat.HP = 100;
+                PlStat.ATK = Random.Shared.Next(10, 15);
+
                 Command = Console.ReadLine().ToLower();
+                // Mechanics(PlStat, Enemy);
 
                 if (Command == "help")
                 {
-                    System.Console.WriteLine("This this command list:");
+                    System.Console.WriteLine("This is the command list:\n[Enter] to Attack.\nWrite 'stat' to veiw your status.\nWrite 'skill' for open skill list.\nWrite 'item' for item list.\nWrite 'run' or 'esc' to escape the enemy to previous floor (Chances up to your SPD).");
+                    System.Console.WriteLine("\n[Enter] to continue.\n");
                     continue;
                 }
-                if(EnemyIsDead == false)
+                if (Command == "stats" || Command == "stat")
                 {
-                    
-                    Mechanics(PlStat, Enemy);
-                    int EnemyAfHP = Enemy.HP - PlStat.ATK;
-                    System.Console.WriteLine($"You deal {PlStat.ATK} damage to enemy!");
-                    System.Console.WriteLine($"Enemy's  HP is now {EnemyAfHP}!");
+                    System.Console.WriteLine($"{PlStat.GetName}' Status:\nHP: [{PlStat.HP}]\nSTR: [10]\nAGI: [{PlStat.AGI}]\nSPD: [{PlStat.SPD}]");
+                    System.Console.WriteLine("\n[Enter] to continue.\n");
+                    continue;
+                }
 
-                    // if (Enemy.HP <= 0)
-                    // {
-                    //     Enemy.IsDead();
-                    //     EnemyIsDead = true;
-            
-                    // }
-                    // else
-                    // {
+                Enemy.HP = Enemy.HP - PlStat.ATK;
+                System.Console.WriteLine($"You deal {PlStat.ATK} damage to the enemy!");
+                System.Console.WriteLine($"Enemy's HP is now {Enemy.HP}!");
+
+                
+                if (!EnemyIsDead)
+                {
+                    EnemyIsDead = Enemy.IsDead();
+                    HeroIsdead = PlStat.IsDead();
+                    if(EnemyIsDead)
+                    {
+                        Floor++;
+                        break;
+                    }
+                    if (HeroIsdead)
+                    {
                         
-                    //     Enemy.ATK = Random.Shared.Next(2,5);
-                    //     int AfHP = PlStat.HP - Enemy.ATK;
-                    //     System.Console.WriteLine($"Enemy deal {Enemy.ATK} damage to you!");
-                    //     System.Console.WriteLine($"your HP is now {AfHP}!");
-                    // }
-
+                        GameOver = true;
+                        break;
+                    }
+                    else
+                    {
                     
-
+                        Enemy.ATK = Random.Shared.Next(2 +(Floor - 1), 5 + (Floor - 1));
+                        PlStat.HP = PlStat.HP - Enemy.ATK;
+                        System.Console.WriteLine($"Enemy deals {Enemy.ATK} damage to you!");
+                        System.Console.WriteLine($"Your HP is now {PlStat.HP}!");
+                    }
                     
                 }
+                
                 
 
             }
         }
     }
+
+    if(mag == true)
+    {
+
+    }
+
+
+
 }
-if(mag == true)
-
-
-
-
 Console.ReadLine();
 
 
@@ -215,18 +280,21 @@ Console.ReadLine();
 
 
 
-static void Mechanics(Char PlStat, Char Enemy)
-{
-    if (PlStat.AGI > Enemy.AGI)
-    {
-        int Chance = PlStat.AGI - Enemy.AGI;
-        int Dodge = Random.Shared.Next(0, Chance);
+// static bool Mechanics(Char PlStat, Char Enemy)
+// {
+//     if (PlStat.SPD > Enemy.SPD)
+//     {
+//         int Chance = PlStat.SPD - Enemy.SPD;
+//         int Dodge = Random.Shared.Next(0, Chance);
 
-        if (Dodge > 5)
-        {
-            System.Console.WriteLine("The enemy missed the attack!");
-            PlStat.ATK = 0;
-        }
-    }
+//         if (Dodge > 5)
+//         {
+//             System.Console.WriteLine("The enemy missed the attack!");
+//             PlStat.ATK = 0;
+//             return true;
+//         }
+        
+//     }
+//     return false;
 
-}
+// }
